1e0e9d4631c0370ad121ab0e303b2b7ea6a9effa
James Lizamore <jameslizamore@perlego.com>
2022-09-23 10:10:17 +0100

    Additional clases

---
+++ blackjack/hand.py
@@ -0,0 +1,13 @@
+"""
+In blackjack a hand is initially dealt as 2 cards
+A player is then able to optionally request additional cards until they 'bust'
+A hand is considered 'bust' if it exceeds the value of 21
+
+Create a hand class which is able to:
+- Holds the state of the hand in terms of which cards it is made up of
+- Can get the current value of the hand (the sum of the ranks)
+- Can determine whether the hand has 'bust' or not (is it over 21?)
+"""
+
+class Hand:
+    ...
---
+++ blackjack/player.py
@@ -0,0 +1,12 @@
+"""
+We want a class to represent a player
+
+A player should have:
+- A balance
+  - The ability to modify the balance
+    - Ensure the balance never drops below 0!
+- A hand
+"""
+
+class Player:
+    ...
